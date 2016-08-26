73d5d18c14160f05eb48ad587f0a1fb50a2953b5
Shaun Hamman <shaunh@slalom.com>
2016-08-26 14:50:40 -0700

    First pass attack calculations

---
+++ Assets/Scripts/AttackCalculator.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Scripts
+{
+	public enum AttackResult
+	{
+		Kill,
+		CounterKill,
+		Push,
+		CounterPush
+	}
+
+	public class AttackCalculator : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		private readonly Random _random;
+
+		public AttackCalculator()
+		{
+			_random = new Random();
+		}
+
+		public AttackResult Calculate(int attack, int defense)
+		{
+			const int sidesOnDie = 6;
+
+			int sum = 0;
+			for (int i = 0; i < attack; i++)
+			{
+				sum += _random.Next(sidesOnDie) + 1;
+			}
+
+			int totalAttack = sum;
+
+			sum = 0;
+			for (int i = 0; i < defense; i++)
+			{
+				sum += _random.Next(sidesOnDie) + 1;
+			}
+
+			int totalDefense = sum;
+
+			if (totalAttack - totalDefense >= 7)
+			{
+				return AttackResult.Kill;
+			}
+			if (totalAttack - totalDefense < 7 && totalAttack - totalDefense > 0)
+			{
+				return AttackResult.Push;
+			}
+			if (totalDefense - totalAttack >= 7)
+			{
+				return AttackResult.CounterKill;
+			}
+
+			return AttackResult.CounterPush;
+		}
+	}
+}
