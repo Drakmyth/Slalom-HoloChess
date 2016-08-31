40873b7daca9e7aebe62c2b039bab8b4143fe8a0
icburns <ianb@slalom.com>
2016-08-30 17:24:10 -0700

    start graph algorithm

---
+++ DejarikLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DejarikLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DejarikLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e93a8c36-8f71-4f25-bd70-793f00bc1f03")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DejarikLibrary/SpaceNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DejarikLibrary
+{
+
+    public class SpaceNode
+    {
+        public int Id { get; set; }
+        public List<SpaceNode> AdjacentNodes { get; set; }
+//        public Transform Position { get; set; }
+
+        public SpaceNode(int id)
+        {
+            Id = id;
+            AdjacentNodes = new List<SpaceNode>();
+        }
+    }
+}
