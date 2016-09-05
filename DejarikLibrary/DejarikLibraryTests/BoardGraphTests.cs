678ad6fe15287fb609b692e6218a1e73933ccd0b
icburns <ebeptonian@gmail.com>
2016-09-05 12:23:53 -0700

    Target library to .net 3.5 to appease unity
    You don't know what you're missing until it's gone.

--- DejarikLibrary/DejarikLibraryTests/BoardGraphTests.cs
+++ DejarikLibrary/DejarikLibraryTests/BoardGraphTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DejarikLibrary;
@@ -20,13 +19,13 @@ namespace DejarikLibraryTests
             for (int i = 13; i < 25; i++)
             {
 
-                List<NodePath> actualNodePath = graph.NodeMap[new Tuple<int, int>(0, 1)];
+                List<NodePath> actualNodePath = graph.NodeMap[new NodeMapKey(0, 1)];
                 Assert.IsTrue(actualNodePath.Select(x => x.DestinationNode).Contains(graph.Nodes[i - 12]));
                 Assert.AreEqual(1, actualNodePath.Single(x => x.DestinationNode.Id == i - 12).PathToDestination.Count());
                 Assert.AreEqual(graph.Nodes[i - 12].Id, actualNodePath.Single(x => x.DestinationNode.Id == i - 12).PathToDestination[0].Id);
 
 
-                actualNodePath = graph.NodeMap[new Tuple<int, int>(0, 2)];
+                actualNodePath = graph.NodeMap[new NodeMapKey(0, 2)];
                 Assert.IsTrue(actualNodePath.Select(x => x.DestinationNode).Contains(graph.Nodes[i]));
                 Assert.AreEqual(2, actualNodePath.Single(x => x.DestinationNode.Id == i).PathToDestination.Count());
                 Assert.AreEqual(graph.Nodes[i].Id, actualNodePath.Single(x => x.DestinationNode.Id == i).PathToDestination[1].Id);
