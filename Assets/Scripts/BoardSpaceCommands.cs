4a9034bf6e291189f33a05b3165bbe9ad7d00533
icburns <ebeptonian@gmail.com>
2016-10-04 02:38:34 -0700

    Improve board space models, add cursor and support for basic gesture recognition

---
+++ Assets/Scripts/BoardSpaceCommands.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BoardSpaceCommands : MonoBehaviour {
+        // Called by GazeGestureManager when the user performs a Select gesture
+        void OnSelect()
+        {
+            var meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+            meshRenderer.material.color = Color.cyan;
+        }
+    }
+}
