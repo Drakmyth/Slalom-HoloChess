494c8f0d2c232e3c78275dbe53965a9e5965dc67
icburns <ebeptonian@gmail.com>
2016-10-11 01:42:39 -0700

    Add menu scene, objects and base animation

---
+++ Assets/Scripts/Menu/MenuBoard.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MenuBoard : MonoBehaviour
+    {
+        private float _currentAlpha;
+        private float _deltaAlpha;
+        private float _rotationSpeed;
+
+        // Use this for initialization
+        void Start()
+        {
+            Material material = gameObject.GetComponent<MeshRenderer>().material;
+            _currentAlpha = 0;
+            material.color = new Color(material.color.r, material.color.g, material.color.b, _currentAlpha);
+            _rotationSpeed = -110;
+            _deltaAlpha = 0;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (_rotationSpeed > -.1 && Mathf.Abs(transform.eulerAngles.x - 300f) < .01)
+            {
+                return;
+            }
+
+            Material material = gameObject.GetComponent<MeshRenderer>().material;
+            _currentAlpha = material.color.a + _deltaAlpha;
+            material.color = new Color(material.color.r, material.color.g, material.color.b, _currentAlpha);
+            transform.Rotate(Vector3.down, _rotationSpeed);
+            _rotationSpeed = _rotationSpeed * .995f;
+
+            if (_currentAlpha < 1)
+            {
+                _deltaAlpha += .000002f;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuEmpire.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MenuEmpire : MonoBehaviour
+    {
+
+
+        private float _currentAlpha;
+        private float _deltaAlpha;
+        private float _rotationSpeed;
+
+        // Use this for initialization
+        void Start()
+        {
+            Material material = gameObject.GetComponent<MeshRenderer>().material;
+            _currentAlpha = material.color.a;
+            _rotationSpeed = -.01f;
+            _deltaAlpha = 0.0001f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Math.Abs(_currentAlpha) < .01)
+            {
+                SendMessageUpwards("OnEmpireFaded");
+            }
+
+            Material material = gameObject.GetComponent<MeshRenderer>().material;
+            _currentAlpha = material.color.a - _deltaAlpha;
+            material.color = new Color(material.color.r, material.color.g, material.color.b, _currentAlpha);
+            transform.Rotate(Vector3.down, _rotationSpeed);
+            _rotationSpeed = _rotationSpeed * 1.02f;
+            _deltaAlpha += .005f * _deltaAlpha;
+        }
+    }
+}
