956beeaf332869bbb4ef1f7977c972431f699782
icburns <ebeptonian@gmail.com>
2016-10-11 01:42:39 -0700

    Add menu scene, objects and base animation

---
+++ Assets/Scripts/Menu/MenuTitle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MenuTitle : MonoBehaviour {
+
+        // Use this for initialization
+        void Start ()
+        {
+            var text = GetComponent<GUIText>();
+
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}
