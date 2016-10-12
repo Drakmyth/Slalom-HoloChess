f3ba44f4d56b4981b07acb8050d67cf3c43d33d1
2D\laura.tuck <laura.tuck@slalom.com>
2016-10-12 16:48:23 -0700

    added attack result text

---
+++ Assets/Scripts/AttackResultText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackResultText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mainCamera = GameObject.Find("Main Camera");
+
+	}
+}
