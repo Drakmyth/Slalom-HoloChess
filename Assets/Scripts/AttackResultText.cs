e3c4353e4e1c893a1a05b08438c24b851b38b0e2
icburns <ianb@slalom.com>
2016-10-13 16:04:44 -0700

    Add attackResultText to battle

--- Assets/Scripts/AttackResultText.cs
+++ Assets/Scripts/AttackResultText.cs
@@ -1,29 +1,43 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class AttackResultText : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class AttackResultText : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+        private Vector3 LerpDestination { get; set; }
+        private const float LerpRate = .2f;
+
+        // Use this for initialization
+        void Start () {
+            gameObject.SetActive(false);	
+        }
 	
-	}
+        // Update is called once per frame
+        void Update () {
+            if (isActiveAndEnabled)
+            {
+                RotateToCamera();
 
-    // Update is called once per frame
-    void Update()
-    {
-        GameObject mainCamera = GameObject.Find("Main Camera");
-        Vector3 relativePos = mainCamera.transform.position - transform.position;
-        Quaternion lookRotation = Quaternion.LookRotation(relativePos);
-        transform.rotation = Quaternion.Euler(lookRotation.eulerAngles.x, lookRotation.eulerAngles.y + 180, lookRotation.eulerAngles.z);
-    }
+                transform.position = Vector3.Lerp(transform.position, LerpDestination, Time.deltaTime*LerpRate);
 
-    void displayResult()
-    {
-        transform.Translate(Vector3.up * Time.deltaTime);
-    }
+            }
+        }
 
-    void hideResult()
-    {
-        transform.Translate(Vector3.down * Time.deltaTime);
+        void OnActive(Vector3 initialPosition)
+        {
+            gameObject.SetActive(true);
+            LerpDestination = initialPosition + Vector3.up * .4f;
+
+        }
+
+        private void RotateToCamera()
+        {
+            GameObject mainCamera = GameObject.Find("Main Camera");
+            Vector3 relativePos = mainCamera.transform.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = Quaternion.Euler(lookRotation.eulerAngles.x, lookRotation.eulerAngles.y + 180, lookRotation.eulerAngles.z);
+
+        }
+
     }
 }
