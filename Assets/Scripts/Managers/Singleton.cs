c74de56afe09068ffa535c6ab5ab855cdf703e7a
icburns <ianb@slalom.com>
2016-11-12 00:07:03 -0800

    Implement gaze stabilizer

---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                }
+                return _instance;
+            }
+        }
+    }
+}
