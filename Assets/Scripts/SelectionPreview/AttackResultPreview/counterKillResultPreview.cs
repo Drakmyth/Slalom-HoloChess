37c5f1d23ee4bed9a80c12daa1c83703af3df528
icburns <ianb@slalom.com>
2016-11-17 23:26:54 -0800

    Add attack result preview handlers

--- Assets/Scripts/SelectionPreview/AttackResultPreview/counterKillResultPreview.cs
+++ Assets/Scripts/SelectionPreview/AttackResultPreview/counterKillResultPreview.cs
@@ -1,15 +1,35 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+using DejarikLibrary;
+using UnityEngine;
 
-public class counterKillResultPreview : MonoBehaviour {
+namespace Assets.Scripts.SelectionPreview.AttackResultPreview
+{
+    public class CounterKillResultPreview : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+        private float InitialX;
+        private float InitialY;
+        private float InitialWidth;
+        private float InitialHeight;
+
+        // Use this for initialization
+        void Start ()
+        {
+            InitialX = GetComponent<RectTransform>().rect.x;
+            InitialY = GetComponent<RectTransform>().rect.y;
+            InitialWidth = GetComponent<RectTransform>().rect.width;
+            InitialHeight = GetComponent<RectTransform>().rect.height;
+        }
+
+        // Update is called once per frame
+        void Update () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        }
+
+        void OnUpdateAttackResultPreview(IDictionary<AttackResult, decimal> attackResults)
+        {
+            float offsetX = ((float)attackResults[AttackResult.Kill] + (float)attackResults[AttackResult.Push] + (float)attackResults[AttackResult.CounterPush]) * InitialX;
+
+            GetComponent<RectTransform>().rect.Set(offsetX, InitialY, InitialWidth * (float)attackResults[AttackResult.CounterPush], InitialHeight);
+        }
+    }
 }
--- Assets/Scripts/SelectionPreview/AttackResultPreview/counterPushResultPreview.cs
+++ Assets/Scripts/SelectionPreview/AttackResultPreview/counterPushResultPreview.cs
@@ -1,15 +1,36 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+using DejarikLibrary;
+using UnityEngine;
 
-public class counterPushResultPreview : MonoBehaviour {
+namespace Assets.Scripts.SelectionPreview.AttackResultPreview
+{
+    public class CounterPushResultPreview : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        private float InitialX;
+        private float InitialY;
+        private float InitialWidth;
+        private float InitialHeight;
+
+        // Use this for initialization
+        void Start()
+        {
+            InitialX = GetComponent<RectTransform>().rect.x;
+            InitialY = GetComponent<RectTransform>().rect.y;
+            InitialWidth = GetComponent<RectTransform>().rect.width;
+            InitialHeight = GetComponent<RectTransform>().rect.height;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnUpdateAttackResultPreview(IDictionary<AttackResult, decimal> attackResults)
+        {
+            float offsetX = ((float)attackResults[AttackResult.Kill] + (float)attackResults[AttackResult.Push]) * InitialX;
+
+            GetComponent<RectTransform>().rect.Set(offsetX, InitialY, InitialWidth * (float)attackResults[AttackResult.CounterPush], InitialHeight);
+        }
+    }
 }
