20fd52e2cc8a7807d9f2336fed96b80465ffc63a
Ainsley Herndon <ainsleyh@slalom.com>
2016-12-23 15:20:42 -0800

    ui is complete

---
+++ Assets/Scripts/Menu/LevelSelectionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SelectLevel(int level)
+    {
+        GameManager.Instance.SinglePlayerButton(level);
+        GameManager.Instance.StartGame();
+    }
+
+}
